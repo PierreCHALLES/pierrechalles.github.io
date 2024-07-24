baa941d85b45107a735dc354a30a652f7f3eef58
PierreCHALLES <pch@mtb111.fr>
2024-07-24 14:48:34 +0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -32,9 +32,9 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       "opacity": {
         "value": 0.5,
-        "random": false,
+        "random": true,
         "anim": {
-          "enable": false,
+          "enable": true,
           "speed": 1,
           "opacity_min": 0.1,
           "sync": false
